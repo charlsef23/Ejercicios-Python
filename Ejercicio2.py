0273eb5e9e5cf45f74072478d7133093cd76d798
Carlos Esteve <charlsef23@icloud.com>
2025-01-18 15:27:40 +0100

    Ejercicio 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,6 +1,6 @@
-"""
-Pide un voto entre A y B y manipula los resultados para que B gane siempre
-"""
+
+# Pide un voto entre A y B y manipula los resultados para que B gane siempre
+
 res = {"A": 10, "B": 10}
 
 voto = input("A o B? ").upper()
