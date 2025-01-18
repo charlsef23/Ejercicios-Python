bde589f9b6c89da09dd24202738b25d69681fde9
Carlos Esteve <charlsef23@icloud.com>
2025-01-18 11:58:22 +0100

    Ejercicio 6

---
+++ Ejercicio6.py
@@ -0,0 +1,11 @@
+"""Ejercicio 6: Número Par o Impar"""
+
+
+
+while True:
+    num = int(input("Escribe un número:"))
+    
+    if num % 2 == 0:
+        print("El número es par")
+    else:
+        print("El número es impar")
